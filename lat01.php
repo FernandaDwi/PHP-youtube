e5f97b46f3631680852e749cbe6c2bab9af1461d
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-02-16 09:46:06 +0700

    vidio 2
    php menampilkan tulisan dan HTML

---
+++ lat01.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "belajar PHP";
+echo '<br>';
+echo'saya siswa smk';
+echo '<h1 style="background-color:blue">belajar php lumayan mudah</h1>
+      <p style="background-color:green">saya suka belajar program</p>
+';
+
+echo 'saya Belajar'.' PHP '.'<br>';
+
+echo 'tahun 2022' ;
+
+?>
