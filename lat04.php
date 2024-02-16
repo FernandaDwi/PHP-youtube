2e5600d3248b956ea5c063ddcdd8defebc2efe80
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-02-16 12:32:14 +0700

    vidio 4
    operator PHP

---
+++ lat04.php
@@ -0,0 +1,55 @@
+<?php
+
+// OPERATOR MATEMATIKA
+
+$a = 5;
+$b = 2;
+
+$c = $a + $b;
+
+echo $c.'<br>';
+
+$c = $a - $b;
+echo $c.'<br>';
+
+$c = $a * $b;
+echo $c.'<br>';
+
+$c = $a / $b;
+echo floor($c).'<br>';
+
+$c = $a % $b;
+echo $c.'<br>';
+
+// OPERATOR LOGIKA
+
+$c = $a < $b;
+echo $c;
+
+$c = $a > $b;
+echo $c;
+
+$c = $a == $b;
+echo $c;
+
+$c = $a != $b;
+echo $c.'<br>';
+
+//INCREMENT
+
+echo $a++;
+echo $a.'<br>';
+
+//OPERATOR STRING
+
+$kata = 'sura';
+$kota = 'baya';
+
+$hasil = $kata.$kota;
+$hasil = $hasil.'kota pahlawan';
+echo $hasil;
+
+
+
+
+?>
