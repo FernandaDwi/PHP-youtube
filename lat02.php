0d1b82a2cb808f988d508faa3faa92910be57efa
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-02-16 10:10:30 +0700

    vidio 3
    variabel tipe data dan komentar

---
+++ lat02.php
@@ -0,0 +1,17 @@
+<?php
+
+$tulisan = 'saya Belajar';
+
+echo $tulisan.'<br>';
+
+$angka = 2019;
+
+echo 'tahun'.$angka.'<br>';
+
+var_dump($tulisan);
+
+echo '<br>';
+
+var_dump((int)$angka+3);
+
+?>
