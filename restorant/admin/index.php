d9fa6ec18811abbede3ae86330b95651772a8b00
FernandaDwi <140361266+FernandaDwi@users.noreply.github.com>
2024-03-24 13:12:52 +0700

    vidio 25
    pembuatan layout aplikasi restoran bootstrap

---
+++ restorant/admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin page | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootsrap.css">
+</head>
+<body>
+    <div class="container">
+         
+    <div class="row">
+           <div class="col-md-3">
+              <h2>Restoran</h2>
+           </div>
+
+           <div class="col-md-9">
+                <div class="float-right mt-4"> logout </div>
+
+           </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-md-3">
+            
+            <ul class="nav flex-column">
+                <li class="nav-item"><a class="nav-link" href="#">kategori</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">user</a></li>
+            </ul>
+        </div>
+        <div class="col-md-9">
+
+        </div>
+
+    </div>
+
+    <div class="row mt-4">
+           <div class="col">
+            <p class="text-center"> 2023-copyright@smknbuduran</p>
+           </div>
+    </div>
+
+    </div>
+</body>
+</html>
